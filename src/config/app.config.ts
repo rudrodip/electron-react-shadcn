5db1fc3c9921f251d49c36795fa09c24a50278ff
rudrodip <official.rudrodipsarker@gmail.com>
2024-10-10 11:49:46 +0600

    init app config

---
+++ src/config/app.config.ts
@@ -0,0 +1,11 @@
+import type { AppConfig } from "@/types";
+
+export const appConfig: AppConfig = {
+  name: "Electron React ShadCN",
+  title: "Electron React ShadCN",
+  description: "Electron React ShadCN",
+  links: {
+    x: "https://x.com/rds_agi",
+    github: "https://github.com/rudrodip/electron-react-shadcn"
+  }
+}
